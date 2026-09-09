58347518c8631d3870c6a89192e6abcb7ae5ca9d
tejesh649 <saitejesh649@gmail.com>
2026-09-09 11:42:49 -0400

    chore: initialize Priceflow cost request service

---
+++ services/cost-request-service/src/main/java/com/priceflow/costrequest/CostRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.priceflow.costrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostRequestServiceApplication.class, args);
+	}
+
+}
